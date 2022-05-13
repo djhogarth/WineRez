99b78b84b03f6246df31131202a39e5765f430ae
DJ Hogarth <dariyhogarth@hotmail.com>
2022-05-12 22:03:11 -0400

    updated the product controller to use the specification pattern and generic repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Domain.Interfaces;
+using Domain.Specifications;
 
 namespace API.Controllers
 {
@@ -10,41 +11,50 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly IProductRepository _productRepository;
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandsRepo;
+        private readonly IGenericRepository<ProductType> _productTypesRepo;
         
-        public ProductsController(IProductRepository productRepository)
+        public ProductsController(IGenericRepository<Product> productsRepo, 
+            IGenericRepository<ProductBrand> productBrandsRepo, 
+            IGenericRepository<ProductType> productTypesRepo)
         {
-            _productRepository = productRepository;
-            
+            _productTypesRepo = productTypesRepo;
+            _productBrandsRepo = productBrandsRepo;
+            _productsRepo = productsRepo;
         }
 
         [HttpGet]
-
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _productRepository.GetProductsAsync();
+            var specification = new ProductsWithTypesAndBrandsSpecification();
+            var products = await _productsRepo.ListAsync(specification);
             return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var specification = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productsRepo.GetEntityWithSpecification(specification);
+            return Ok(product);
         }
         
         [HttpGet("brands")]
 
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-            var brands = await _productRepository.GetProductBrandsAsync();
+            var brands = await _productBrandsRepo.ListAllAsync();
             return Ok(brands);
         }
 
          [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            var types = await _productRepository.GetProductTypesAsync();
+            var types = await _productTypesRepo.ListAllAsync();
             return Ok(types);
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _productRepository.GetProductByIdAsync(id);
-        }
+        
     }
 }
