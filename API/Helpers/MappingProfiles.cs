0e99de945534467de1ffbeb9185ae8c31717fd9a
DJ Hogarth <dariyhogarth@hotmail.com>
2022-05-16 23:40:47 -0400

    added a helper class that uses a custom value resolver to map from the picture url to api url + picture url

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -14,7 +14,9 @@ namespace API.Helpers
         {
             CreateMap<Product, ProductDTO>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
                                 
         }
     }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,26 @@
+using API.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductDTO, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
