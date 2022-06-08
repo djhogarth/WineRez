0dbdf78b61f392f67b908af8d639547d2f670e9d
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-08 12:19:10 -0400

    added swagger documentation and excluded error controller from  the documentation

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
    [Route("errors/{errorCode}")]
+   [ApiExplorerSettings(IgnoreApi = true)]
    public class ErrorController : BaseApiController
     {
         public IActionResult Error(int errorCode)
