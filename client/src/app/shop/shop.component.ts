590dcb5ff994d56a0c5edb0344b2d477305b89c0
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-16 03:20:54 -0400

    resolving an error when the user on page 2 and then clicks a filter, two network requests are made instead of one. To fix this, only run what's in the onPageChanged method if an actual page is changed using the pager and not when clicking a filter.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -102,8 +102,11 @@ export class ShopComponent implements OnInit
 
   onPageChanged(event: any)
   {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if(this.shopParams.pageNumber != event)
+    {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch()
