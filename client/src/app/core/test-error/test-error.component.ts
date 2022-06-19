7394c67190ffc44f6c408e594a5ed537bdc2ebbf
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-19 05:31:06 -0400

    created a test error component for testing error responses in the client/angular

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit
+{
+  baseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void
+  {
+
+  }
+
+  get404Error()
+  {
+    this.http.get(this.baseUrl + 'products/70').subscribe( response =>
+      {
+        console.log(response);
+      }, error =>
+      {
+        console.log(error);
+      });
+  }
+
+  get500Error()
+  {
+    this.http.get(this.baseUrl + 'errorresponsetest/serverError').subscribe( response =>
+      {
+        console.log(response);
+      }, error =>
+      {
+        console.log(error);
+      });
+  }
+
+  get400Error()
+  {
+    this.http.get(this.baseUrl + 'errorresponsetest/badRequest').subscribe( response =>
+      {
+        console.log(response);
+      }, error =>
+      {
+        console.log(error);
+      });
+  }
+
+  get400ValidationError()
+  {
+    this.http.get(this.baseUrl + 'products/seventy').subscribe( response =>
+      {
+        console.log(response);
+      }, error =>
+      {
+        console.log(error);
+      });
+  }
+
+}
