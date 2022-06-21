8107d6e1c9ca941ead141090fe28ec03cd3108ec
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-20 21:54:19 -0400

    added breadcrumbs using the xng-breadcrumbs package.

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
   styleUrls: ['./section-header.component.scss']
 })
-export class SectionHeaderComponent implements OnInit {
+export class SectionHeaderComponent implements OnInit
+{
+  breadcrumb$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private breadCrumbService: BreadcrumbService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    this.breadcrumb$ = this.breadCrumbService.breadcrumbs$;
   }
 
 }
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,7 +12,8 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private breadCrumbService: BreadcrumbService) { }
 
   ngOnInit(): void
   {
@@ -20,9 +22,10 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct()
   {
-    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(response =>
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(returnedProduct =>
       {
-        this.product = response;
+        this.product = returnedProduct;
+        this.breadCrumbService.set('@productDetails', returnedProduct.name)
       }, error =>
       {
         console.log(error);
