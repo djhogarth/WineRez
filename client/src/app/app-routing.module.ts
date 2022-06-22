bca1ce1287a997192793ce58cf787c7a43a5b060
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-22 16:02:07 -0400

    created the basket feature module and it's relevant files on the client side

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ const routes: Routes =
   {path: 'test-error', component: TestErrorComponent, data: {breadcrumb : 'Test Error'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
     data: {breadcrumb : 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+    data: {breadcrumb : 'Shopping Cart'}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full', data: {breadcrumb : 'Not Found'}}
 
 ];
