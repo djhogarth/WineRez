3d7c840fee50e43954ba75d7d21f6351ecce1200
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-24 17:18:50 -0400

    setting up identity entity classes

---
+++ Domain/Entities/Identity/Address.cs
@@ -0,0 +1,17 @@
+namespace Domain.Entities.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetNumberAndName { get; set; }
+        public string City { get; set; }    
+        public string StateOrProvince { get; set; }
+        public string ZipCode { get; set; }
+
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
