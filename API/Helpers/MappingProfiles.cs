1389d9fb6c91cb58097799b62d9737b842fc3e2b
DJ Hogarth <dariyhogarth@hotmail.com>
2022-06-30 18:03:54 -0400

    Telling automapper how to map the cetain properties to resolve errors when mapping

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -15,6 +15,7 @@ namespace API.Helpers
         public MappingProfiles()
         {
             CreateMap<Product, ProductDTO>()
+            //Telling automapper how to map the cetain properties (d = ProductDTO, o = Product)
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
@@ -23,9 +24,14 @@ namespace API.Helpers
             CreateMap<CustomerBasketDTO, CustomerBasket>();
             CreateMap<BasketItemDTO, BasketItem>();
             CreateMap<AddressDTO, Domain.Entities.OrderAggregate.Address>();
-            CreateMap<Order, OrderToReturnDTO>();
-            CreateMap<OrderItem, OrderItemDTO>();
+            CreateMap<Order, OrderToReturnDTO>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
 
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl));
 
         }
     }
