901a7910c9542940560e62d1f8326d395210b422
DJ Hogarth <dariyhogarth@hotmail.com>
2022-07-01 12:53:45 -0400

    added a method to get the delivery methods in the checkout service

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CheckoutService {
+export class CheckoutService
+{
+  private baseUrl = environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDeliveryMethods()
+  {
+    return this.http.get(this.baseUrl + 'order/deliveryMethods').pipe(
+      map((deliveryMethods: IDeliveryMethod []) =>
+      {
+        return deliveryMethods.sort((a,b) => b.price - a.price);
+      })
+    );
+  }
 }
